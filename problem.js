52490d62c44e2542c1964b587eb09791d8f5e4be
narsa8058 <narsa8058@gmail.com>
2022-11-30 13:29:22 +0530

    login_reg

---
+++ problem.js
@@ -0,0 +1,28 @@
+let num=9;
+let count=0;
+for(i=1;i<=num;i++){
+if(num%i==0){
+count++
+}
+}
+if(count==2){
+console.log("num is prime no.")
+}
+else{
+console.log("num is not prime no.")
+
+let num=221122;
+let str= ""+ num;
+l=str.length
+m=Math.floor(l/2)
+let count=0;
+for(i=0;i<=m;i++){
+if(str[i]!==str[(l-1)-i]{
+count++
+}
+}
+if(count>=1){
+console.log("num is not palindrome")
+}else{
+console.log("num is palindrome")
+}
